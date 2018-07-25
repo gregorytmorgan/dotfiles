82a65a5b9016b6258ba3120c1eee400470f81b7a
gmorgan <gmorgan@soleranetworks.com>
2018-07-25 08:09:36 -0600

    Update setup to use symlinks

--- setup.sh
+++ setup.sh
@@ -1,13 +1,44 @@
+#!/bin/bash
 
-cp bashrc ~/.bashrc
+TSTAMP=`/bin/date '+%Y-%m-%d.%s'`
 
-cp bash_aliases ~/.bash_aliases
+DOT_ROOT=`/bin/pwd`
 
-cp profile ~/.profile
+# work from home dir
+cd ~
 
-if [ -e ~/.emacs.d ]; then
-    cp -fr emacs.d/* ~/.emacs.d
-else
-    cp -r emacs.d ~/.emacs.d    
+# bashrc
+if [ -e .bashrc ] || [ -L .bashrc ]; then
+    /bin/mv .bashrc .bashrc.$TSTAMP
 fi
+/bin/ln -s $DOT_ROOT/bashrc .bashrc 
+
+# bash_aliases
+if [ -e .bash_aliases ] || [ -L .bash_aliases ]; then
+    /bin/mv .bash_aliases .bash_aliases.$TSTAMP
+fi
+/bin/ln -s $DOT_ROOT/bash_aliases .bash_aliases
+
+# profile
+if [ -e .profile ] || [ -L .profile ]; then
+    /bin/mv .profile .profile.$TSTAMP
+fi
+/bin/ln -s $DOT_ROOT/profile .profile
+
+# bash_logout
+if [ -e .bash_logout ] || [ -L .bash_logout ]; then
+    /bin/mv .bash_logout .bash_logout.$TSTAMP
+fi
+/bin/ln -s $DOT_ROOT/bash_logout .bash_logout
+
+# emacs
+if [ -e .emacs.d ] || [ -L .emacs.d ]; then
+    /bin/mv .emacs.d .emacs.d.$TSTAMP
+fi
+/bin/ln -s $DOT_ROOT/emacs.d .emacs.d
+
+cd $DOT_ROOT
+
+# end file
+
 
